31ae5cc0d99d7831ba9008e67f3d9244037cbd21
rushikesh.shende <r.shende@aia.rwth-aachen.de>
2026-07-01 12:32:51 +0200

    last place and update telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,76 +1,98 @@
 return {
   {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  dependencies = {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = {
       'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope-ui-select.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
-  config = function()
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-    vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = 'Find recent files' })
-    vim.keymap.set('n', '<leader>jl', builtin.jumplist, { desc = 'Show Jumplist entries' })
-    vim.keymap.set('n', '<leader>fcg', function()
-      local opts = {}  -- Options table
+    config = function()
+      local telescope = require('telescope')
+      local builtin = require('telescope.builtin')
 
-      local case_sensitive = vim.fn.input("Case sensitive? (y/n): ")
-      if case_sensitive == "n" then
-        opts['grep_open_files'] = false
-        opts['additional_args'] = function()
-          return { "--ignore-case" }  -- Add case-insensitive flag
-        end
-      end
+      -- Define the explicit path to your standalone ripgrep binary
+      local custom_rg_path = vim.fn.expand("$HOME") .. "/.local/bin/rg"
 
-      local whole_words = vim.fn.input("Whole words only? (y/n): ")
-      if whole_words == "y" then
-        opts['additional_args'] = function()
-          return { "--word-regexp" }  -- Add whole word flag
-        end
-      end
-
-      local file_pattern = vim.fn.input("File pattern (e.g., src/FV/*.cpp): ")
-      if file_pattern ~= "" then
-        opts['search_dirs'] = { file_pattern }
-      end
-
-      require('telescope.builtin').live_grep(opts)
-    end, { desc = 'Telescope live grep with custom options' })
-    vim.keymap.set("n", "<leader>ts", require("telescope.builtin").lsp_document_symbols, { noremap = true, silent = true })
-  end
-  },
-  {
-  'nvim-telescope/telescope-ui-select.nvim',
-  config = function()
-      -- This is your opts table
-      require("telescope").setup {
+      telescope.setup({
+        defaults = {
+          -- 1. Point Telescope explicitly to your local ripgrep binary without changing $PATH
+          vimgrep_arguments = {
+            custom_rg_path,
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+          },
+          -- 2. Performance tweaks for shared HPC network filesystems
+          file_ignore_patterns = { "node_modules", "%%.git/", "target/", "build/", "dist/" },
+        },
         extensions = {
           ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-              -- even more opts
-            }
-
-            -- pseudo code / specification for writing custom displays, like the one
-            -- for "codeactions"
-            -- specific_opts = {
-            --   [kind] = {
-            --     make_indexed = function(items) -> indexed_items, width,
-            --     make_displayer = function(widths) -> displayer
-            --     make_display = function(displayer) -> function(e)
-            --     make_ordinal = function(e) -> string
-            --   },
-            --   -- for example to disable the custom builtin "codeactions" display
-            --      do the following
-            --   codeactions = false,
-            -- }
+            require("telescope.themes").get_dropdown {}
+          },
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
           }
         }
-      }
-      -- To get ui-select loaded and working with telescope, you need to call
-      -- load_extension, somewhere after setup function:
-      require("telescope").load_extension("ui-select")
-  end
+      })
 
+      telescope.load_extension("ui-select")
+      telescope.load_extension("fzf")
+
+      -- Standard Keymaps (These will now use your isolated 'rg' binary seamlessly)
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = 'Find recent files' })
+      vim.keymap.set('n', '<leader>jl', builtin.jumplist, { desc = 'Show Jumplist entries' })
+      vim.keymap.set("n", "<leader>ts", builtin.lsp_document_symbols, { noremap = true, silent = true })
+
+      -- Custom Interactive Live Grep Mapping (Also updated to cleanly pass args)
+      vim.keymap.set('n', '<leader>fcg', function()
+        local args = {}
+        local opts = {}
+
+        -- 1. Case Sensitive prompt (Instant keypress)
+        print("Case sensitive? (y/n): ")
+        local case_sensitive = vim.fn.getcharstr()
+        if case_sensitive == "n" or case_sensitive == "N" then
+          table.insert(args, "--ignore-case")
+        end
+
+        -- 2. Whole Words prompt (Instant keypress)
+        print("Whole words only? (y/n): ")
+        local whole_words = vim.fn.getcharstr()
+        if whole_words == "y" or whole_words == "Y" then
+          table.insert(args, "--word-regexp")
+        end
+
+        local file_pattern = vim.fn.input("File pattern/folder (e.g., src/ or *.cpp): ")
+        if file_pattern ~= "" then
+          if file_pattern:sub(-1) == "/" then
+            file_pattern = file_pattern .. "**/*"
+          elseif vim.fn.isdirectory(file_pattern) == 1 then
+            file_pattern = file_pattern .. "/**/*"
+          end
+
+          table.insert(args, "-g")
+          table.insert(args, file_pattern)
+        end
+
+        if #args > 0 then
+          opts.additional_args = function()
+            return args
+          end
+        end
+
+        builtin.live_grep(opts)
+      end, { desc = 'Telescope live grep with custom options' })
+    end
   }
 }
--- lua/vimOptions.lua
+++ lua/vimOptions.lua
@@ -11,6 +11,12 @@ vim.cmd("nnoremap <C-l> :set number!<CR>")
 vim.cmd("set autoindent")
 vim.cmd("set smartindent")
 
+-- Navigate windows using Space + Arrow keys
+vim.keymap.set('n', '<leader><Left>',  '<C-w>h', { desc = 'Move to left window' })
+vim.keymap.set('n', '<leader><Down>',  '<C-w>j', { desc = 'Move to lower window' })
+vim.keymap.set('n', '<leader><Up>',    '<C-w>k', { desc = 'Move to upper window' })
+vim.keymap.set('n', '<leader><Right>', '<C-w>l', { desc = 'Move to right window' })
+
 vim.cmd("set foldmethod=indent")
 -- vim.cmd("set foldlevel=0")
 vim.opt.foldlevel = 99
@@ -35,3 +41,23 @@ vim.keymap.set("n", "<leader>fs", function()
   vim.cmd("e " .. vim.fn.expand("%:r") .. "." .. ext)
 end, { noremap = true, silent = true })
 
+-- Create an augroup for managing file views (folds, cursor position, etc.)
+local view_group = vim.api.nvim_create_augroup("RememberFolds", { clear = true })
+
+-- Save fold settings when leaving a buffer
+vim.api.nvim_create_autocmd("BufWinLeave", {
+  group = view_group,
+  pattern = "?*", -- Matches files with actual names, ignores empty/scratch buffers
+  callback = function()
+    vim.cmd("silent! mkview")
+  end,
+})
+
+-- Restore fold settings when entering a buffer
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = view_group,
+  pattern = "?*",
+  callback = function()
+    vim.cmd("silent! loadview")
+  end,
+})
