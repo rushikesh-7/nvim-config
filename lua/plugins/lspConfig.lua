2642f53c10d573ffe9b02144695aa84aadc8b08f
Rushikesh Shende <rushikesh.shende@gmail.com>
2026-07-03 13:43:42 +0200

    clangd on hunter

--- lua/plugins/lspConfig.lua
+++ lua/plugins/lspConfig.lua
@@ -1,28 +1,44 @@
-return{
+return {
   {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require('lspconfig')
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
       lspconfig.lua_ls.setup({
         capabilities = capabilities,
       })
-      -- local root_dir = lspconfig.util.root_pattern("compile_commands.json", ".git")(vim.fn.expand("%:p"))
-      -- local compile_commands_dir = root_dir or "/home/rushikesh/Desktop/master_maia/Solver"
+
       lspconfig.clangd.setup({
         cmd = {
           "clangd",
-          "--compile-commands-dir=/home/rushikesh/Desktop/master_maia/Solver",
-          -- "--compile-commands-dir" .. compile_commands_dir,
-          "--background-index",  -- Enables project-wide symbol indexing
-          "--clang-tidy",        -- Enables extra linting
+          "--background-index",          -- Enables project-wide symbol indexing
+          "--clang-tidy",                -- Enables extra linting
           "--completion-style=detailed", -- More precise completion
-          "--header-insertion=iwyu",  -- "Include What You Use" (suggests necessary headers)
-          "--cross-file-rename",  -- Allows renaming across multiple files
+          "--header-insertion=iwyu",     -- "Include What You Use"
+          "--cross-file-rename",         -- Allows renaming across multiple files
         },
         filetypes = {"c", "cpp", "objc", "objcpp", "h"},
         root_dir = lspconfig.util.root_pattern("compile_commands.json", ".git"),
         capabilities = capabilities,
+
+        -- DYNAMICALLY FIND COMPILE_COMMANDS.JSON FOR EACH BUFFER
+        on_new_config = function(new_config, new_root_dir)
+          local fallback_dir = "/home/rushikesh/Desktop/master_maia/Solver"
+
+          -- Search upwards from the current file for compile_commands.json
+          local match = vim.fs.find("compile_commands.json", { 
+            path = new_root_dir, 
+            upward = true, 
+            stop = vim.env.HOME 
+          })[1]
+
+          -- If found, use its parent directory. If not, use fallback_dir.
+          local compile_commands_dir = match and vim.fs.dirname(match) or fallback_dir
+
+          -- Inject the argument dynamically into this instance of clangd
+          table.insert(new_config.cmd, "--compile-commands-dir=" .. compile_commands_dir)
+        end,
         on_attach = function(client, bufnr)
           require("nvim-navic").attach(client, bufnr)
         end
@@ -31,11 +47,7 @@ return{
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('UserLspConfig', {}),
         callback = function(ev)
-          -- Enable completion triggered by <c-x><c-o>
           vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-          -- Buffer local mappings.
-          -- See `:help vim.lsp.*` for documentation on any of the below functions
           local opts = { buffer = ev.buf }
           vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
           vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
@@ -50,24 +62,25 @@ return{
     end
   },
   {
-  "williamboman/mason.nvim",
-  config = function()
-    require("mason").setup({
-      ui = {
-        icons = {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
             package_installed = "✓",
             package_pending = "➜",
             package_uninstalled = "✗"
+          }
         }
-      }
-  })
-  end
+      })
+    end
   },
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-      ensure_install = {"lua_ls", "cpp", "c", "pylsp", "bash", "make", "cmake", "texlab"}
+        -- FIXED: changed "c" and "cpp" to "clangd"
+        ensure_installed = {"lua_ls", "clangd", "pylsp", "bashls", "cmake", "texlab"}
       })
     end
   }
