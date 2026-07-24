0b02b6a23ba27ef194e3f3a59dc749488370d891
rushikesh.shende <rushikesh.shende@rwth-aachen.de>
2026-07-24 14:44:43 +0200

    add markdown support

--- lua/plugins/treeSitter.lua
+++ lua/plugins/treeSitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html", "make", "python", "cmake", "cpp", "bash", "toml"},
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html", "make", "python", "cmake", "cpp", "bash", "toml", "markdown", "markdown_inline"},
       highlight = { enable = true},
       indent = { enable = true },
     })
