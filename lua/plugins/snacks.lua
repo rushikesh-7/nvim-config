cb51b24ea139db6bb3b20e4fc70421aedd379c23
rushikesh.shende <rushikesh.shende@rwth-aachen.de>
2026-08-31 11:59:51 +0200

    switch from telescope to snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -35,12 +35,60 @@ return {
     },
 
     input = { enabled = true },
-    -- picker = { enabled = false },
+    picker = {
+      enabled = true,
+      ui_select = true,
+    },
     notifier = { enabled = true },
     quickfile = { enabled = true },
     scope = { enabled = true },
     -- scroll = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
-  }
+  },
+  keys = {
+    { '<leader>ff', function() Snacks.picker.files() end, desc = 'Find files' },
+    { '<leader>fg', function() Snacks.picker.grep() end, desc = 'Live grep' },
+    { '<leader>fb', function() Snacks.picker.buffers() end, desc = 'Buffers' },
+    { '<leader>fh', function() Snacks.picker.help() end, desc = 'Help tags' },
+    { '<leader>fr', function() Snacks.picker.recent() end, desc = 'Find recent files' },
+    { '<leader>fw', function() Snacks.picker.grep_word() end, desc = 'Find word under cursor' },
+    { '<leader>jl', function() Snacks.picker.jumps() end, desc = 'Show Jumplist entries' },
+    { '<leader>ts', function() Snacks.picker.lsp_symbols() end, desc = 'Document symbols' },
+    {
+      '<leader>fcg',
+      function()
+        local args = {}
+        local glob
+
+        -- 1. Case Sensitive prompt (Instant keypress)
+        print("Case sensitive? (y/n): ")
+        local case_sensitive = vim.fn.getcharstr()
+        if case_sensitive == "n" or case_sensitive == "N" then
+          table.insert(args, "--ignore-case")
+        end
+
+        -- 2. Whole Words prompt (Instant keypress)
+        print("Whole words only? (y/n): ")
+        local whole_words = vim.fn.getcharstr()
+        if whole_words == "y" or whole_words == "Y" then
+          table.insert(args, "--word-regexp")
+        end
+
+        local file_pattern = vim.fn.input("File pattern/folder (e.g., src/ or *.cpp): ")
+        if file_pattern ~= "" then
+          if file_pattern:sub(-1) == "/" then
+            file_pattern = file_pattern .. "**/*"
+          elseif vim.fn.isdirectory(file_pattern) == 1 then
+            file_pattern = file_pattern .. "/**/*"
+          end
+
+          glob = file_pattern
+        end
+
+        Snacks.picker.grep({ args = args, glob = glob })
+      end,
+      desc = 'Live grep with custom options',
+    },
+  },
 }
